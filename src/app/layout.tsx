257a80ed52a340a214c0c489db54c4fc9a38a58d
Lars Faye <lars@goodchee.com>
2024-04-28 22:01:16 -0400

    renaming Nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
-import AppNav from "./components/appnav";
+import AppNav from "./components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
